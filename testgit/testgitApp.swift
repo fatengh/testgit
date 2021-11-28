65d618ed2b41fcc5d95356304a04fad3708276e1
fatengh <faten.arg@hotmail.com>
2021-11-28 18:31:41 +0000

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by admin on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
